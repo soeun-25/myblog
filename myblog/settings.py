c5aa0e361361f585a749859ef1b80963bf812857
KimSoEun <kimsoeun0225@naver.com>
2020-07-01 19:29:13 +0900

    setting.py update

--- myblog/settings.py
+++ myblog/settings.py
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'k0-kr'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = Asia/Seoul''
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
