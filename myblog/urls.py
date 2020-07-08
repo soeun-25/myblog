14fda22c37f524f3f0f21e64afd8b935bb076da9
KimSoEun <kimsoeun0225@naver.com>
2020-07-08 20:10:27 +0900

    글쓰기

--- myblog/urls.py
+++ myblog/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+import introduction.urls
+import posts.urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('introduction/', include('introduction.urls')),
     path('', views.main, name="main"),
+    path('posts/', include('posts.urls')),
 ]
---
+++ posts/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import *
+
+
+app_name="posts"
+
+urlpatterns = [
+    path('new/', new, name="new"),
+    path('create/', create, name="create"),
+    path('', main, name="main"),
+    path('show/<int:id>/', show, name="show"),
+]
+
